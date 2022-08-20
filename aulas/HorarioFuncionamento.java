ef06838fabb046efe40a20fa14aed6b7ef9b3038
lesoame <leunip_2010@hotmail.com>
2022-08-20 14:14:45 -0300

    swuitch with break

---
+++ aulas/HorarioFuncionamento.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class HorarioFuncionamento {
+
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+  
+    System.out.print("Digite um dia da semana (ex: seg, ter, qua, etc): ");
+    String diaSemana = entrada.nextLine();
+    String horarioFuncionamento;
+
+    switch (diaSemana) {
+      case "seg":
+        horarioFuncionamento = "Fechado";
+        break;
+      case "ter":
+      case "qua":
+      case "qui":
+      case "sex":
+        horarioFuncionamento = "08:00 as 18:00";
+        break;
+      case "sab":
+      case "dom":
+        horarioFuncionamento = "08:00 as 12:00";
+        break;
+      default:
+        horarioFuncionamento = "Dia invalido";
+    }  
+  
+    System.out.printf("Horario de funcionamento: %s%n", horarioFuncionamento);
+ 
+  }
+
+}
